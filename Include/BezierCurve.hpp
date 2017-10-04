2d20082c59239f21725c85090f015fd43faf0080
Thomas Margier <thomas.margier@live.fr>
2017-10-04 15:56:49 +0200

    Courbe de bezier homogène ok

---
+++ Include/BezierCurve.hpp
@@ -0,0 +1,22 @@
+﻿/**
+ * \file BezierCurve.hpp
+ * \brief todo
+ * \author Thomas Margier
+ * \version 1.0
+ * \date 04/00/17
+ * \copyright Copyright (c) 2017 Thomas Margier
+ *  This file is licensed under the MIT License, see https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+#include <vector>
+#include "Vec4.hpp" //todo : fix issue when including vec3 before vec4
+#include "Vec3.hpp"
+
+namespace Math
+{
+	struct BezierCurve
+	{
+		static std::vector<Vec3d> ComputeWithDeCasteljau(const std::vector<Vec4d>& controlPoints, unsigned nbPoints);
+	};
+}
---
+++ Source/BezierCurve.cpp
@@ -0,0 +1,36 @@
+﻿#include "BezierCurve.hpp"
+#include "Vec2.hpp"
+
+std::vector<Math::Vec3d> Math::BezierCurve::ComputeWithDeCasteljau (
+	const std::vector<Vec4d>& controlPoints, const unsigned nbPoints)
+{
+	std::vector<Vec3d> curve(nbPoints + 2);
+	curve[0] = Vec3d(controlPoints[0]);
+	curve[curve.size() - 1] = Vec3d(controlPoints[controlPoints.size() - 1]);
+
+	std::vector<Vec4d> temp_points(controlPoints.size());
+	
+	const auto step = 1.0 / (double(nbPoints) + 1.0);
+	size_t index = 1;
+
+	for (auto u = step; u < 1 && index < curve.size(); u += step, ++index)
+	{
+		for (unsigned i = 0; i < controlPoints.size(); ++i)
+		{
+			temp_points[i] = controlPoints[i].w * controlPoints[i];
+			temp_points[i].w = controlPoints[i].w;
+		}
+
+		for (unsigned j = 1; j < controlPoints.size(); ++j)
+		{
+			for (unsigned i = 0; i < controlPoints.size() - j; ++i)
+			{
+				temp_points[i] = (1 - u) * temp_points[i] + u* temp_points[i + 1];
+			}
+		}
+
+		curve[index] = Vec3d(temp_points[0]) / temp_points[0].w;
+	}
+
+	return move(curve);
+}
